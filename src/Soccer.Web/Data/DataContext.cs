d395b837eca16a2fdd3e604a681ed85df287a7b9
Isaac <isaac.rebolledoleal@gmail.com>
2020-03-27 14:09:40 -0300

    Tournaments added

--- src/Soccer.Web/Data/DataContext.cs
+++ src/Soccer.Web/Data/DataContext.cs
@@ -9,8 +9,16 @@ namespace Soccer.Web.Data
 {
     public class DataContext : DbContext
     {
+        public DbSet<GroupDetailEntity> GroupDetails { get; set; }
+
+        public DbSet<GroupEntity> Groups { get; set; }
+
+        public DbSet<MatchEntity> Matches { get; set; }
 
         public DbSet<TeamEntity> Teams { get; set; }
+
+        public DbSet<TournamentEntity> Tournaments { get; set; }
+
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         { }
